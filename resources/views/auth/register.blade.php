29fa491154391b4fbbc87f21058624fb88dbad1e
Ricardo-Olais <r.hernandez@lidcorp.mx>
2022-10-21 12:18:19 -0500

    ajuste login y registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,15 +14,15 @@
         <div class="card-content">
             <center><h2 class="card-title mb-0"><b>Registro a Gps Tracker</b></h2></center>
 
-            <div class="card-body">
+            <div class="card-body" style="margin-top:20px;">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Nombre completo</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right" style="font-size:18px;">Nombre completo</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus style="font-size:18px;">
 
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
@@ -33,10 +33,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">Correo Electrónico</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right" style="font-size:18px;">Correo Electrónico</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" style="font-size:18px;">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -47,10 +47,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Contraseña</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right" style="font-size:18px;">Contraseña</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" style="font-size:18px;">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -61,7 +61,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">Confirmar Contraseña</label>
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right" style="font-size:18px;">Confirmar Contraseña</label>
 
                             <div class="col-md-6">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
@@ -70,7 +70,7 @@
 
                         <div class="row">
                             <div class="col s12 m2 l12">
-                                <button type="submit" class="waves-effect waves-light btn" style="width: 100%;">
+                                <button type="submit" class="waves-effect waves-light btn" style="width: 100%;height: 50px;font-size: 20px;">
                                     Registrarme<i class="material-icons right">chevron_right</i>
                                 </button>
                             </div>
@@ -86,8 +86,8 @@
   <div class="row">
 
     <div class="row vertical-modern-dashboard">
-        <div class="col s12 m2 l12 animate fadeRight">
-            <div class="card">
+        <div class="col s12 m2 l12 animate fadeRight" style="background-color:#000;">
+            <div class="card" style="background-color:#000;color:#fff;">
                  <div class="card-content">
             Estás a unos pasos de disfrutar de los beneficios que localizaminave tiene para ti, rastrea tus dispositivos de forma segura, activa  geocercas, alertas de movimiento y mucho más...
                 </div>
