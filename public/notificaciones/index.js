42b417121b531dc008b21940f4f8075c9ca9e848
Ricardo-Olais <r.hernandez@lidcorp.mx>
2022-10-26 20:07:57 -0500

    ajuste

--- public/notificaciones/index.js
+++ public/notificaciones/index.js
@@ -6,12 +6,20 @@ const io = require('socket.io')(server,{
 });
 io.on('connection',function (socket){
     console.log('connected');
+
+
    socket.on('send-message',function (data){
        console.log(data);
        io.emit('message',data);
-   })
+   });
+
+   socket.on('send-ubicacion',function (data){
+       console.log(data);
+       io.emit('ubicacion',data);
+   });
+
    socket.on('disconnect',function (){
        console.log('disconnected')
-   })
+   });
 })
 server.listen(3000);
