15de36b51838b94607dc42ef34ddf28c58489e72
Ricardo-Olais <r.hernandez@lidcorp.mx>
2022-11-07 17:32:14 -0600

    ajuste

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -3,8 +3,14 @@
 @section('content')
  
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin=""/>
-<script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+<link rel="stylesheet" href="css/leaflet.css"/>
+<script src="js/leaflet.js"></script>
+
+
+<!--script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script-->
+
+
+
 <script src="notificaciones/node_modules/socket.io-client/dist/socket.io.js"></script>
 
   <style>
@@ -75,6 +81,11 @@
             50% { opacity: 0.0; }
            100% { opacity: 1.0; }
          }
+
+        /* .leaflet-control-attribution{
+
+          display: none;
+         }*/
   </style>
 
 
