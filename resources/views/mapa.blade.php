32f7efad201109ebffe4e79d46b1f31b68020454
Ricardo-Olais <r.hernandez@lidcorp.mx>
2022-12-10 11:33:20 -0600

    zoom

--- resources/views/mapa.blade.php
+++ resources/views/mapa.blade.php
@@ -610,6 +610,8 @@ L.control.condensedAttribution({
       //https://www.google.com/maps?layer=c&cbll=19.5441708,-99.0785885
     //  L.streetView().addTo(map);
 
+      map.setZoom(15);
+
       theMarker = L.marker([msg.longitud, msg.latitud],{icon: customIcon, draggable: true,
     autoPan: true}).addTo(map).bindPopup('<b>Dispositivo '+msg.alias+' se encuentra en </b><br />'+msg.direccion+ ', conductor: '+msg.conductor).openPopup();
 
