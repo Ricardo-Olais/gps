bca6f9cf5329e001db2cf2e144af416f623a0555
Ricardo-Olais <r.hernandez@lidcorp.mx>
2023-01-25 13:31:17 -0600

    ajustes

--- public/facturas.php
+++ public/facturas.php
@@ -10,7 +10,7 @@ $subscripcion=$_REQUEST['id'];//$_REQUEST['subscripcion'];
 //$id=$_REQUEST['id'];
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-$stripe = new \Stripe\StripeClient('sk_test_51IRNcvK9KYNU2QdIqItH66j09101WtH82MSBoeB79AscKj5AlWPFAA8GracCTPijDYvekImzxyoQpmaEOCkY37Gn00a3O0hoHA');
+$stripe = new \Stripe\StripeClient('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');
 
 $resul=$stripe->subscriptions->retrieve(
   "$subscripcion",
@@ -21,7 +21,7 @@ $id=$resul['customer'];
 
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-$stripe = new \Stripe\StripeClient('sk_test_51IRNcvK9KYNU2QdIqItH66j09101WtH82MSBoeB79AscKj5AlWPFAA8GracCTPijDYvekImzxyoQpmaEOCkY37Gn00a3O0hoHA');
+$stripe = new \Stripe\StripeClient('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');
 
 $stripe->billingPortal->configurations->create(
   [
