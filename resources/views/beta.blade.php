dbc51e373e15f97877f95ae888be25444ab63417
Ricardo-Olais <r.hernandez@lidcorp.mx>
2023-09-04 14:01:55 -0600

    ajustes

--- resources/views/beta.blade.php
+++ resources/views/beta.blade.php
@@ -524,7 +524,7 @@ socket.on('ubicacion', function(msg) {
         }
 
         //{className: "custom-popup"}
-    const popup = L.popup()
+    const popup = L.popup({className: "custom"})
     .setLatLng([msg.longitud, msg.latitud])
     .setContent(msg.alias+ "<center><img src='https://localizaminave.com/img/"+msg.tipo+"' style='width: 20px; height: 30px;'></center>")
     .openOn(map);
