a13ab96383bde3ad9a812c160210ad5a5556abb4
Ricardo-Olais <r.hernandez@lidcorp.mx>
2023-09-08 16:36:44 -0600

    aaa

--- resources/views/beta.blade.php
+++ resources/views/beta.blade.php
@@ -503,7 +503,7 @@ socket.on('ubicacion', function(msg) {
 
         var greenIcon = new L.Icon({
           iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-          shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
+          shadowUrl: 'https://localizaminave.com/img/iconper.png',
           iconSize: [25, 41],
           iconAnchor: [12, 41],
           popupAnchor: [1, -34],
